bf88c6a5577607929febb941bbaee846e068c6a9
adnan <adnan@ibos.io>
2025-03-02 12:31:38 +0600

    Async

--- src/main/java/com/api/erp/common/EmailService.java
+++ src/main/java/com/api/erp/common/EmailService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,7 +15,7 @@ public class EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
-
+    @Async
     public void sendEmail(String to, String subject, String htmlBody) throws MessagingException {
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
