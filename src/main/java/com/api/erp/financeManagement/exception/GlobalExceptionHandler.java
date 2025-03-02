901ef72d7dbcbad1c7c3fdd54d96a91a8975adf6
adnan <adnan@ibos.io>
2025-03-02 13:07:33 +0600

    finance management fix

--- src/main/java/com/api/erp/financeManagement/exception/GlobalExceptionHandler.java
+++ src/main/java/com/api/erp/financeManagement/exception/GlobalExceptionHandler.java
@@ -2,10 +2,11 @@ package com.api.erp.financeManagement.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@Component("financeExceptionHandler")
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
